e53569411b1d4cfa685de7f3eef7dfd1d2152c2a
= <=>
2021-03-25 20:26:10 +0700

    add file main.java

--- src/main.java
+++ src/main.java
@@ -4,5 +4,11 @@ public class main {
         System.out.print("Hello World");
 
         //1. Tạo thư mục .git
+        // commit : phát tín hiệu
+
+        // (fix) git config --global user.name "first name lasts name"
+        //       git config --global user.emal "my_name@Exanoke.com"
+
+
     }
 }
