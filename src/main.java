2a8f1b7a449374f25810a8cc0d4517ff9bb06735
= <=>
2021-03-25 21:02:27 +0700

    kiểu dữ liệu

--- src/main.java
+++ src/main.java
@@ -1,23 +1,37 @@
+
+//1. Tạo thư mục .git
+// git init
+
+//2. xem su thay đổi
+//git status
+
+//3, lưu trữ sự thay đổi
+//git add .
+
+//4 lưu trữ vào local repo
+//git commit -m "ten message"
+
+
+//        git remote add origin https://github.com/giakhanh97/javacoban23032021.git
+//        git branch -M main
+//        git push -u origin main
+
+
 public class main {
 
     public static void main(String [] args) throws Exception{
         System.out.print("Hello World");
 
-        //1. Tạo thư mục .git
-        // git init
-
-        //2. xem su thay đổi
-        //git status
-
-        //3, lưu trữ sự thay đổi
-        //git add .
-
-        //4 lưu trữ vào local repo
-        //git commit -m "ten message"
-
-
-
-
+//        1: comment code
+//        System.out.print"Hello World"
+//
+//        2. Ki?u d? li?u
+//                -NHÓM KI?U D? LI?U NGUYÊN TH?Y : Không h? tr? phương th?c x? l?
+//                Viết thường chữ đầu	int , boolean , double , float , char , ....
+//
+//
+//        -NHÓM KI?U D? LI?U Đ?I TƯ?NG :	 H? tr? các phương th?c x? ly
+//        Viết hoa chữ đầu 	String , Int , Boolean , Double , Float , CharSequence .....
 
     }
 }
